8f2207de9b65eaa4d9e9cd38c1505f3941065cfb
Eojin0208 <amvnfr@gmail.com>
2024-05-18 21:50:15 +0900

    2nd modal
    메인: 애니메이션, 폰트, 배경 이미지
    
    프로젝트리스트: 진행중인, 완성된 구분, +버튼(새로운 프로젝트 생성, 초대코드 입력) 구현

---
+++ src/Main.js
@@ -0,0 +1,25 @@
+import './Main.css';
+
+function Main() {
+
+  return (
+
+    <div>
+      
+      <div className = "M-Background"></div>
+          
+      <div className = "M-Your">
+        너의
+      </div>
+
+      <div className = "M-Schedule">
+        일정은
+      </div>
+
+      <div className = "M-Button">시작하기</div>
+
+    </div>
+  );
+}
+
+export default Main;
