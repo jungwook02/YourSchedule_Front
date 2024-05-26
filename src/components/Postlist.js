e7c673b53310e8ac3ea69668f3f3ee6171da5a80
Eojin0208 <amvnfr@gmail.com>
2024-05-26 14:21:02 +0900

    2nd Modal
    Postlist(게시판 목록) 추가

---
+++ src/components/Postlist.js
@@ -0,0 +1,48 @@
+import "./Postlist.css"
+
+function Postlist(){
+  return(
+    <div>
+        <div className="Postlist-Title">
+            <div className="Post-Text">
+                게시판
+            </div>
+            <div className="Title-Text">
+                모바일 프로그래밍
+            </div>
+        </div>
+
+        <input type="text" class="Search-input" placeholder="검색" />
+
+        <div className="Postlist-Box">
+            
+            <div className="Postlist-Preview">
+                <div className="Postlist-Previewtitle">5/28 발표 자료</div>
+                <div className="Postlist-Previewinfo">
+                    <div className="Postlist-Previewpublisher">박한비</div>
+                    <div className="Postlist-Previewtime">오늘 13:05</div>
+                </div>
+            </div>
+
+            <div className="Postlist-Preview">
+                <div className="Postlist-Previewtitle">WBS</div>
+                <div className="Postlist-Previewinfo">
+                    <div className="Postlist-Previewpublisher">이어진</div>
+                    <div className="Postlist-Previewtime">오늘 10:43</div>
+                </div>
+            </div>
+
+            <div className="Postlist-Preview">
+                <div className="Postlist-Previewtitle">개발 진행 상황</div>
+                <div className="Postlist-Previewinfo">
+                    <div className="Postlist-Previewpublisher">김섭진</div>
+                    <div className="Postlist-Previewtime">어제 10:43</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default Postlist;
