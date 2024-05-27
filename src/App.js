e9d23728c959637eadb3ec05760c19c94936f0f8
Eojin0208 <amvnfr@gmail.com>
2024-05-27 12:13:34 +0900

    2nd Modal
    Postlist, Post 화면 추가했습니다

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Main from"./components/Main.js";
-import Projectlist from "./components/Projectlist.js";
 import Login from "./components/Login.js"
-import Projectmain from "./components/Projectmain.js"
 import Membership from "./components/Membership.js"
+import Projectlist from "./components/Projectlist.js";
+import Projectmain from "./components/Projectmain.js"
+import Postlist from "./components/Postlist.js"
+import Post from "./components/Post.js"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
 
-    <Projectmain />
+      <Post />
 
     </div>
   );
