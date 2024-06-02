cd347bd7871544fe42c0aecd1354936f54273869
Eojin0208 <amvnfr@gmail.com>
2024-06-02 17:35:51 +0900

    2nd Modal
    너의 일정은 생성 화면 추가

---
+++ src/components/YourSchedlueCreation.js
@@ -0,0 +1,27 @@
+import "./YourSchedlueCreation.css"
+
+function YourSchedlueCreation() {
+    return(
+        <div>
+            <div className="YourSchedule-Title">
+                <div className="YourSchedule-Text">
+                    너의 일정은
+                </div>
+                <div className="Title-Text">
+                    모바일 프로그래밍
+                </div>
+            </div>
+
+            <div className="YourSchedule-Box">
+            <input type="text" class="Post-Title-input" placeholder="주제를 입력해주세요" />
+            
+            <div className="PostCreation-Button">게시하기</div>
+            </div>
+
+            
+        </div>
+    )
+
+}
+
+export default YourSchedlueCreation;
